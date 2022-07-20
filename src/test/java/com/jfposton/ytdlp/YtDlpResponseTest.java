4d4a27c7823e19bff468d1ad152519ad00ed544f
Jonathan Poston <jonathan.poston@archivesocial.com>
2022-07-20 11:51:21 -0400

    test: update the unit tests

--- src/test/java/com/jfposton/ytdlp/YtDlpResponseTest.java
+++ src/test/java/com/jfposton/ytdlp/YtDlpResponseTest.java
@@ -1,12 +1,18 @@
 package com.jfposton.ytdlp;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
 import org.junit.Test;
+
+import com.jfposton.ytdlp.mapper.VideoFormat;
 
 public class YtDlpResponseTest {
 
   @Test
-  public void testTest() {
-    Assert.assertEquals(true, true);
+  public void getFormatsCanHandleLargerVideos() throws YtDlpException {
+    List<VideoFormat> videoFormats = YtDlp.getFormats(String.format("https://www.youtube.com/watch?v=XNSM5RtUgXQ"));
+    assertTrue(videoFormats.get(0).getUrl().contains("XNSM5RtUgXQ"));
   }
 }
--- src/test/java/com/jfposton/ytdlp/YtDlpTestSuite.java
+++
@@ -1,8 +0,0 @@
-package com.jfposton.ytdlp;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({YtDlpTest.class, YtDlpRequestTest.class})
-public class YtDlpTestSuite {}
