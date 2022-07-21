9fe832ee497ca198a93b31f43c07d626a7940387
Jonathan Poston <jonathan.poston@archivesocial.com>
2022-07-21 14:57:33 -0400

    chore: fix miscommited file

--- src/test/java/com/jfposton/ytdlp/YtDlpResponseTest.java
+++ src/test/java/com/jfposton/ytdlp/YtDlpResponseTest.java
@@ -16,15 +16,9 @@ public class YtDlpResponseTest {
 
   @Test
   public void getFormatsCanHandleLargerVideos() throws YtDlpException {
-<<<<<<< HEAD
-    List<VideoFormat> videoFormats =
-        YtDlp.getFormats("https://www.youtube.com/watch?v=jPTO3lcPpik");
-    assertTrue(videoFormats.get(0).getUrl().contains("jPTO3lcPpik"));
-=======
     VideoInfo videoInfo = YtDlp.getVideoInfo("https://www.youtube.com/watch?v=jPTO3lcPpik");
     assertEquals("jPTO3lcPpik", videoInfo.getId());
     assertNotNull(videoInfo.getFormats());
     assertFalse(videoInfo.getFormats().isEmpty());
->>>>>>> 51c864c (tests: improve approach to larger videos test)
   }
 }
